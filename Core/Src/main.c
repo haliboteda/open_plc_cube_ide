4febbd6b4e11ed36940a9c7edc83616d95ecc4a1
Whoami <419182047@qq.com>
2025-04-26 15:53:29 +0800

    1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,7 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "crc.h"
-#include "mbedtls.h"
+#include "lwip.h"
 #include "rtc.h"
 #include "usart.h"
 #include "usb_device.h"
@@ -234,42 +234,44 @@ uint8_t Check_BOOT0_Pressed(void) {
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
 
-	/* USER CODE BEGIN 1 */
-	/* USER CODE END 1 */
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_MBEDTLS_Init();
-	MX_USB_DEVICE_Init();
-	MX_UART4_Init();
-	MX_RTC_Init();
-	MX_CRC_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USB_DEVICE_Init();
+  MX_UART4_Init();
+  MX_RTC_Init();
+  MX_CRC_Init();
+  MX_LWIP_Init();
+  /* USER CODE BEGIN 2 */
 	Enable_RX_RS232();
 
-	printf("** Checking Starting Mod ...\r\n"
-		   "** (IF You want OpenPLC to stay in upload mode, please hold down the BOOT0 button for 3-5 seconds while clicking)\r\n");
+	printf(
+			"** Checking Starting Mod ...\r\n"
+					"** (IF You want OpenPLC to stay in upload mode, please hold down the BOOT0 button for 3-5 seconds while clicking)\r\n");
 
 	// Einschalten aller HSFETs einmalig und dann ausschalten
 	for (RELAY_Name relay = RELAY_1; relay < RELAY_COUNT / 2; ++relay) {
@@ -277,120 +279,124 @@ int main(void) {
 		HAL_Delay(500); // Einschaltverzoegerung von 500 Millisekunden
 		Relay_Off(relay);
 	}
+//
+//	// check Upload Mod situation
+//	// 1 MAGIC_BKP_REG is MAGIC_BOOTLOADER_FLAG
+//	// 2 CDC_APP_ADDRESS is empty
+//	// 3 CDC_APP_ADDRESS is not empty and Boot0 button is pressed
+//	uint8_t isBoot0Pressed = Check_BOOT0_Pressed();
+//	uint32_t readData = HAL_RTCEx_BKUPRead(&hrtc, MAGIC_BKP_REG);
+//	if (readData == MAGIC_BOOTLOADER_FLAG
+//			|| ((*(__IO uint32_t*) CDC_APP_ADDRESS) & 0x2FFE0000) != 0x24080000
+//			|| (isBoot0Pressed > 0 && ((*(__IO uint32_t*) CDC_APP_ADDRESS) & 0x2FFE0000) == 0x24080000)) {
+//		printf("** UPLOAD Mod ...\r\n");
+//		printf("** Please input your command: \r\n");
+//		HAL_PWR_EnableBkUpAccess();
+//		HAL_RTCEx_BKUPWrite(&hrtc, MAGIC_BKP_REG, MAGIC_APP_FLAG);
+//		HAL_PWR_DisableBkUpAccess();
+//	} else {
+//		printf("** APP Mod ...\r\n");
+//		/* Jump to user application */
+//		JumpAddress = *(__IO uint32_t*) (CDC_APP_ADDRESS + 4);
+//
+//		JumpToApplication = (pFunction) JumpAddress;
+//
+//		MX_USB_DEVICE_DeInit();
+//		HAL_RCC_DeInit();
+//		HAL_DeInit();
+//
+//		// DeInitializing systick peripheral
+//		SysTick->CTRL = 0;
+//		SysTick->LOAD = 0;
+//		SysTick->VAL = 0;
+//
+//		/* Initialize application Stack Pointer */
+//		__set_MSP(*(__IO uint32_t*) CDC_APP_ADDRESS);
+//		JumpToApplication();
+//	}
 
-	// check Upload Mod situation
-	// 1 MAGIC_BKP_REG is MAGIC_BOOTLOADER_FLAG
-	// 2 CDC_APP_ADDRESS is empty
-	// 3 CDC_APP_ADDRESS is not empty and Boot0 button is pressed
-	uint8_t isBoot0Pressed = Check_BOOT0_Pressed();
-	uint32_t readData = HAL_RTCEx_BKUPRead(&hrtc, MAGIC_BKP_REG);
-	if (readData == MAGIC_BOOTLOADER_FLAG
-			|| ((*(__IO uint32_t*) CDC_APP_ADDRESS) & 0x2FFE0000) != 0x24080000
-			|| (isBoot0Pressed > 0 && ((*(__IO uint32_t*) CDC_APP_ADDRESS) & 0x2FFE0000) == 0x24080000)) {
-		printf("** UPLOAD Mod ...\r\n");
-		printf("** Please input your command: \r\n");
-		HAL_PWR_EnableBkUpAccess();
-		HAL_RTCEx_BKUPWrite(&hrtc, MAGIC_BKP_REG, MAGIC_APP_FLAG);
-		HAL_PWR_DisableBkUpAccess();
-	} else {
-		printf("** APP Mod ...\r\n");
-		/* Jump to user application */
-		JumpAddress = *(__IO uint32_t*) (CDC_APP_ADDRESS + 4);
+  /* USER CODE END 2 */
 
-		JumpToApplication = (pFunction) JumpAddress;
-
-		MX_USB_DEVICE_DeInit();
-		HAL_RCC_DeInit();
-		HAL_DeInit();
-
-		// DeInitializing systick peripheral
-		SysTick->CTRL = 0;
-		SysTick->LOAD = 0;
-		SysTick->VAL = 0;
-
-		/* Initialize application Stack Pointer */
-		__set_MSP(*(__IO uint32_t*) CDC_APP_ADDRESS);
-		JumpToApplication();
-	}
-
-	/* USER CODE END 2 */
-
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1) {
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 		// ready to write flash
-		if (DataReadyFlag > 0) {
-			DataReadyFlag = 0;
-			//printf("%d %d, data is ready LenInRXBuffer %d : receivedBytes %d of %d \r\n", DataReadyFlag, currentState, LenInRXBuffer,receivedBytes, bytesToReceive);
-			LenInFlashBuffer = LenInRXBuffer;
-			LenInRXBuffer = 0;
-			memcpy(FlashBuffer, RXBuffer, LenInFlashBuffer);
-			process_command();
-		}
+//		if (DataReadyFlag > 0) {
+//			DataReadyFlag = 0;
+//			//printf("%d %d, data is ready LenInRXBuffer %d : receivedBytes %d of %d \r\n", DataReadyFlag, currentState, LenInRXBuffer,receivedBytes, bytesToReceive);
+//			LenInFlashBuffer = LenInRXBuffer;
+//			LenInRXBuffer = 0;
+//			memcpy(FlashBuffer, RXBuffer, LenInFlashBuffer);
+//			process_command();
+//		}
+
+		MX_LWIP_Process();
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Supply configuration update enable
-	 */
-	HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  /** Supply configuration update enable
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
-	}
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48
-			| RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 2;
-	RCC_OscInitStruct.PLL.PLLN = 12;
-	RCC_OscInitStruct.PLL.PLLP = 2;
-	RCC_OscInitStruct.PLL.PLLQ = 2;
-	RCC_OscInitStruct.PLL.PLLR = 2;
-	RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
-	RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOMEDIUM;
-	RCC_OscInitStruct.PLL.PLLFRACN = 0;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_LSI
+                              |RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 2;
+  RCC_OscInitStruct.PLL.PLLN = 64;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1
-			| RCC_CLOCKTYPE_D1PCLK1;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-	RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -398,16 +404,17 @@ void SystemClock_Config(void) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
--- Core/Src/stm32h7xx_it.c
+++ Core/Src/stm32h7xx_it.c
@@ -54,6 +54,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern ETH_HandleTypeDef heth;
 extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 /* USER CODE BEGIN EV */
 
@@ -197,6 +198,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32h7xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles Ethernet global interrupt.
+  */
+void ETH_IRQHandler(void)
+{
+  /* USER CODE BEGIN ETH_IRQn 0 */
+
+  /* USER CODE END ETH_IRQn 0 */
+  HAL_ETH_IRQHandler(&heth);
+  /* USER CODE BEGIN ETH_IRQn 1 */
+
+  /* USER CODE END ETH_IRQn 1 */
+}
 
 /**
   * @brief This function handles USB On The Go FS global interrupt.
